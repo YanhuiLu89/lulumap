047f88c0d87d8593777af789ad5c4add11dc92d4
luyanhui <luyanhui@navinfo.com>
2019-12-31 16:55:39 +0800

    修改文件路径

--- app/src/main/java/com/xiaochuang/lulumap/MapLayerActivity.java
+++ app/src/main/java/com/xiaochuang/lulumap/MapLayerActivity.java
@@ -14,17 +14,7 @@ private ListView mListView;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_layer);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        mListView=findViewById(R.id.listView);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 
 }
